62921cb6f41487b32fe124cf39fcbedf039e2c65
Najwa005 <najwafitriyani05@gmail.com>
2024-03-13 11:56:55 +0800

    ubah index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Najwa</title>
+</head>
+<body>
+    <h1>Najwa Fitriyani</h1>
+    <h2>NIM : E020322107</h2>
+</body>
+</html>
